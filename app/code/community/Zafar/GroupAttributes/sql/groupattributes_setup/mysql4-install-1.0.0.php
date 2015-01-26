d6d314d7bc5717f9e5076b39260c96e1ef5cc2f4
Mohammed Zafar <zafar.asn@gmail.com>
2015-01-26 07:03:57 +0300

    sql script

---
+++ app/code/community/Zafar/GroupAttributes/sql/groupattributes_setup/mysql4-install-1.0.0.php
@@ -0,0 +1,17 @@
+<?php
+$installer = $this;
+$installer->startSetup();
+$installer->run("
+	CREATE TABLE `{$this->getTable('eav_frontend_attribute_group_label')}` (
+  	`attribute_group_label_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  	`attribute_group_label` varchar(255) NOT NULL DEFAULT '',
+  	`attribute_group_id` smallint(5) unsigned NOT NULL DEFAULT '0',
+  	`store_id` smallint(5) unsigned NOT NULL DEFAULT '0',
+	PRIMARY KEY (`attribute_group_label_id`),
+  	KEY `FK_eav_frontend_attribute_group_label_store` (`store_id`),
+  	KEY `FK_eav_frontend_attribute_group_label_group` (`attribute_group_id`),
+  	CONSTRAINT `FK_eav_frontend_attribute_group_label_group` FOREIGN KEY (`attribute_group_id`) REFERENCES `{$this->getTable('eav_attribute_group')}` (`attribute_group_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  	CONSTRAINT `FK_eav_frontend_attribute_group_label_store` FOREIGN KEY (`store_id`) REFERENCES `{$this->getTable('core_store')}`   (`store_id`) ON DELETE CASCADE ON UPDATE CASCADE
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+");
+$installer->endSetup();
